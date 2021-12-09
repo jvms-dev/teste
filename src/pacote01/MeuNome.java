7e00c022e47e792f31301533249aabd859f920ee
jvms-dev <joaovmsantos.dev@gmail.com>
2021-12-08 22:31:00 -0300

    mais um commit

--- src/pacote01/MeuNome.java
+++ src/pacote01/MeuNome.java
@@ -5,5 +5,6 @@ public class MeuNome {
     public static void main(String[] args) {
         Projetao meuProjeto = new Projetao(5, 6, 7, 8, 9);
         System.out.println(meuProjeto.getSomaValores());
+        // Joao
     }
 }
